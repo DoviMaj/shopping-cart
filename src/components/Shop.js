c03584ce910bb25f0d7d2ff5dd50d0c482397ff6
dovimaj <dovimajowka@gmail.com>
2020-11-04 21:01:11 +0200

    added opacity effect to shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -19,15 +19,7 @@ const Shop = () => {
   }, [cart]);
 
   return (
-    <div
-      style={{
-        background: "rgb(30, 27, 27)",
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        gap: "30px",
-      }}
-    >
+    <div>
       {cartClicked ? (
         <Checkout
           cartLength={cartLength}
@@ -35,66 +27,78 @@ const Shop = () => {
           setCartClicked={setCartClicked}
         />
       ) : null}
-
       <div
-        onClick={() => {
-          setCartClicked(!cartClicked);
+        className="shop-page"
+        style={{
+          background: "rgb(30, 27, 27)",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "30px",
+          transition: "all 0.3s ease-in-out",
+          WebkitFilter: cartClicked ? "blur(2px) grayscale(10%)" : null,
         }}
-        className="cart-icon"
       >
-        <span>{cart.length}</span>
-        <img
-          alt="cart-icon"
-          src="https://img.icons8.com/dotty/45/000000/shopping-cart.png"
-        />
-      </div>
-      {cars.map((car) => {
-        return (
-          <Card
-            className="car-container"
-            key={car.id}
-            id={car.id}
-            style={{
-              opacity: "0",
-              background: "none",
-              width: "50rem",
-              color: "white",
-              fontFamily: "title",
-              border: "0px",
-            }}
-          >
-            <Card.Img variant="top" src={car.image} />
-            <Card.Body style={{ position: "relative", bottom: "80px" }}>
-              <h1>{car.name}</h1>
-              <Card.Text>{car.description}</Card.Text>
-              <div style={{ display: "flex", gap: "10px" }}>
-                <Button
-                  className="add-to-cart"
-                  onClick={(e) => addToCart(e, car)}
-                >
-                  Add to cart
-                </Button>
-                <FormControl
-                  min="0"
-                  className="amount-input"
-                  style={{ width: "55px" }}
-                  type="number"
-                  defaultValue="1"
-                ></FormControl>
+        <div
+          onClick={() => {
+            setCartClicked(!cartClicked);
+          }}
+          className="cart-icon"
+        >
+          <span>{cart.length}</span>
+          <img
+            alt="cart-icon"
+            src="https://img.icons8.com/dotty/45/000000/shopping-cart.png"
+          />
+        </div>
+        {cars.map((car) => {
+          return (
+            <Card
+              className="car-container"
+              key={car.id}
+              id={car.id}
+              style={{
+                opacity: "0",
+                background: "none",
+                width: "50rem",
+                color: "white",
+                fontFamily: "title",
+                border: "0px",
+              }}
+            >
+              <Card.Img variant="top" src={car.image} />
+              <Card.Body style={{ position: "relative", bottom: "80px" }}>
+                <h1>{car.name}</h1>
+                <Card.Text>{car.description}</Card.Text>
+                <div style={{ display: "flex", gap: "10px" }}>
+                  <Button
+                    className="add-to-cart"
+                    onClick={(e) => addToCart(e, car)}
+                  >
+                    Add to cart
+                  </Button>
+                  <FormControl
+                    min="0"
+                    className="amount-input"
+                    style={{ width: "55px" }}
+                    type="number"
+                    defaultValue="1"
+                  ></FormControl>
+                </div>
+              </Card.Body>
+              <div>
+                <div
+                  style={{
+                    height: "1px",
+                    width: "100%",
+                    background: "white",
+                  }}
+                ></div>
               </div>
-            </Card.Body>
-            <div>
-              <div
-                style={{
-                  height: "1px",
-                  width: "100%",
-                  background: "white",
-                }}
-              ></div>
-            </div>
-          </Card>
-        );
-      })}
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 };
